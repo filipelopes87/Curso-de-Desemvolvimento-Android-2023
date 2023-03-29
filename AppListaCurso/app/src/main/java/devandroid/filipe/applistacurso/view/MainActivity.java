beba8d46a1fe9557b007b89275ea235e2b683ef0
filipelopes87 <filipedagogia@gmail.com>
2023-03-29 08:35:06 -0300

    Uso de Métodos Get e Set

--- AppListaCurso/app/src/main/java/devandroid/filipe/applistacurso/view/MainActivity.java
+++ AppListaCurso/app/src/main/java/devandroid/filipe/applistacurso/view/MainActivity.java
@@ -10,6 +10,11 @@ import devandroid.filipe.applistacurso.model.Pessoa;
 public class MainActivity extends AppCompatActivity {
 
     Pessoa pessoa;
+    Pessoa outraPessoa;
+
+    String dadosPessoa;
+    String dadosOutraPessoa;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +25,33 @@ public class MainActivity extends AppCompatActivity {
         pessoa.setSobreNome("Lopes");
         pessoa.setCursoDesejado("Android");
         pessoa.setTelefoneContato("16 991288574");
+
+        outraPessoa = new Pessoa();
+        outraPessoa.setPrimeiroNome("Paulo");
+        outraPessoa.setSobreNome("Rocha");
+        outraPessoa.setCursoDesejado("Java");
+        outraPessoa.setTelefoneContato("16 885285525");
+
+        dadosPessoa = "Primeiro Nome: ";
+        dadosPessoa += pessoa.getPrimeiroNome();
+        dadosPessoa += " Sobrenome: ";
+        dadosPessoa += pessoa.getSobreNome();
+        dadosPessoa += " Curso Desejado; ";
+        dadosPessoa += pessoa.getCursoDesejado();
+        dadosPessoa += " Telefone de Contato: ";
+        dadosPessoa += pessoa.getTelefoneContato();
+
+        dadosOutraPessoa = "Primeiro Nome: ";
+        dadosOutraPessoa += outraPessoa.getPrimeiroNome();
+        dadosOutraPessoa += " Sobrenome: ";
+        dadosOutraPessoa += outraPessoa.getSobreNome();
+        dadosOutraPessoa += " Curso Desejado; ";
+        dadosOutraPessoa += outraPessoa.getCursoDesejado();
+        dadosOutraPessoa += " Telefone de Contato: ";
+        dadosOutraPessoa += outraPessoa.getTelefoneContato();
+
+
+
+
     }
 }
