d00e4c35d2e95305f77010b411385361f27a3954
filipelopes87 <filipedagogia@gmail.com>
2023-04-04 14:17:57 -0300

    App Feliz Aniversário

---
+++ FelizAniversrio/app/src/main/java/devandroid/filipe/felizaniversrio/MainActivity.kt
@@ -0,0 +1,86 @@
+package devandroid.filipe.felizaniversrio
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import devandroid.filipe.felizaniversrio.ui.theme.FelizAniversárioTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FelizAniversárioTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background,
+                    ) {
+                    BirthdayGreetingWithImage(message = getString(R.string.feliz_aniversario_text), from = stringResource(
+                                            R.string.from_Emma_text)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun BirthdayGreetingWithText(message: String, from: String, modifier: Modifier = Modifier) {
+    Column(
+           modifier = modifier.fillMaxSize(),
+           verticalArrangement = Arrangement.Top,
+           horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = message,
+            fontSize = 36.sp,
+            modifier = Modifier
+                .padding(top = 16.dp)
+        )
+        Text(
+            text = from,
+            fontSize = 24.sp,
+            modifier = Modifier
+                .padding(top = 16.dp)
+                .align(alignment = Alignment.End)
+
+        )
+    }
+}
+@Composable
+
+fun BirthdayGreetingWithImage(message: String, from: String, modifier: Modifier = Modifier){
+    val image = painterResource(id = R.drawable.androidparty__1_)
+    Box {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop
+        )
+        BirthdayGreetingWithText(message = message, from = from)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BirthdayCardPreview() {
+    FelizAniversárioTheme() {
+        BirthdayGreetingWithImage(stringResource(id = R.string.feliz_aniversario_text), stringResource(
+            id = R.string.from_Emma_text
+        ))
+    }
+}
